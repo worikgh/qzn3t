7a5b87e63cebc0671785ff00ce4293c2f813eae7
QZT <5387413+worikgh@users.noreply.github.com>
2024-06-16 17:16:58 +1200

    Initial

---
+++ lv2_ctl/src/modhost_resp.rs
@@ -0,0 +1,41 @@
+/// Object to handle a response, and call a function as a result
+pub trait ModhostResp {
+   /// Pass the complete mod-host response in `resp`.  The UI
+   /// interprets the result from this function to adjust itself
+   fn act_on_response(&self, resp: &str) -> String;
+}
+
+/// The result for a `param_get` requets to get a Port setting
+pub struct ModhostGet {
+   /// LV2 instance number
+   instance: usize,
+   port_idx: usize,
+}
+
+impl ModhostResp for ModhostGet {
+   fn act_on_response(&self, resp: &str) -> String {
+      "".to_string()
+   }
+}
+
+impl ModhostGet {
+   /// The first integer in the response is <=0 except when a
+   /// response to a `param_add` when it is the instance number of
+   /// the added simulator,  
+   fn validate_resp(&self, resp_code: isize) -> bool {
+      if let Ordering::Greater = 0.cmp(&resp_code) {
+         return false;
+      }
+      true
+   }
+
+   fn get_resp_code(resp: &str) -> isize {
+      let r = &resp[5..];
+      let sp: usize = r.chars().position(|x| x.is_whitespace()).unwrap_or(
+         // No whitespace, till end of string
+         r.len(),
+      );
+      let res = r[..sp].trim();
+      res.parse::<isize>().unwrap()
+   }
+}	 
