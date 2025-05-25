15f8c1a341dff1607faae61bf1f254611f4e6ce9
Worik <5387413+worikgh@users.noreply.github.com>
2025-05-26 00:52:11 +1200

    Use standard tool chain

--- peak_volume/src/main.rs
+++ peak_volume/src/main.rs
@@ -1,11 +1,8 @@
 // peak_volume
-#![feature(buf_read_has_data_left)]
 //! Output the greatest amplitude in the passed file.  It is the
 //! difference between the maximum and the minimum amplitude
-//! Use: rustup override set nightly
 use std::env;
 use std::fs;
-use std::io::BufRead;
 use std::io::BufReader;
 use std::io::Read;
 
@@ -21,21 +18,27 @@ fn main() {
     let mut buffer = [0_u8; 4];
     let mut max: f32 = f32::MIN;
     let mut min: f32 = f32::MAX;
+
     loop {
         match reader.read_exact(&mut buffer) {
-            Ok(b) => b,
-            Err(err) => panic!("{err}: Cannot get 4 bytes"),
+            Ok(_) => {
+                let bits = u32::from_le_bytes(buffer);
+                let f = f32::from_bits(bits);
+                if f > max {
+                    max = f;
+                }
+                if f < min {
+                    min = f;
+                }
+            }
+            Err(err) => {
+                // Check if error is due to EOF
+                if err.kind() == std::io::ErrorKind::UnexpectedEof {
+                    break;
+                }
+                panic!("{err}: Cannot read 4 bytes from {in_fn}");
+            }
         };
-        let bits = u32::from_le_bytes(buffer);
-        let f = f32::from_bits(bits);
-        if f > max {
-            max = f;
-        } else if f < min {
-            min = f;
-        }
-        if !reader.has_data_left().unwrap() {
-            break;
-        }
     }
     println!("{}", max - min);
 }
