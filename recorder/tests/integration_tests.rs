cdcea3035882c82b66de1145b0adfb3f62a3db84
Worik <5387413+worikgh@users.noreply.github.com>
2026-02-09 12:19:39 +1300

    recorder: Simplify integration tests

--- recorder/tests/integration_tests.rs
+++ recorder/tests/integration_tests.rs
@@ -15,7 +15,7 @@ use qzn3t_recorder::{
 };
 
 use std::{
-    sync::{Arc, Mutex, atomic::Ordering, mpsc},
+    sync::{Arc, Mutex, atomic::Ordering},
     thread,
     time::Duration,
 };
@@ -104,211 +104,118 @@ fn record_two_channels_and_play_back() {
         let rec_tri = recorder.recorded_audio.get_buffer(1).unwrap();
         let rec_tri = trim_audio(&rec_tri);
 
-        // Reset this on failed tests
-        let mut result = true;
-
         // The two recorded buffers must be identical to the source buffers
-        if rec_sine.len() != audio_buffer_sine.len() {
-            eprintln!(
-                "* Error: Sine lengths differ: recorded: {} Original: {} audio_duration: {audio_duration}",
-                rec_sine.len(),
-                audio_buffer_sine.len()
-            );
-            result = false;
-        } else {
-            for i in 0..audio_buffer_sine.len() {
-                if (rec_sine[i] - audio_buffer_sine[i]).abs() > f32::EPSILON {
-                    eprintln!(
-                        "* Error: Sine differs at {i}, {:0.4}, {:0.4} audio_duration: {audio_duration}",
-                        rec_sine[i], audio_buffer_sine[i]
-                    );
-                    result = false;
-                    break;
+        assert_eq!(rec_sine.len(), audio_buffer_sine.len());
+        assert_eq!(rec_tri.len(), audio_buffer_tri.len());
+        for i in 0..audio_buffer_sine.len() {
+            assert!((rec_sine[i] - audio_buffer_sine[i]).abs() < f32::EPSILON);
+        }
+        for i in 0..audio_buffer_tri.len() {
+            assert!((rec_tri[i] - audio_buffer_tri[i]).abs() < f32::EPSILON);
+        }
+
+        // Good so far.  The recorded buffers match the input.  Now
+        // check the recorded files are the same
+
+        let audio_path = recorder.file_manager.make_paths().unwrap().0;
+        let metadata_path = recorder.file_manager.make_paths().unwrap().1;
+        let metadata: Metadata = match read_file_metadata(metadata_path) {
+            Ok(p) => p,
+            Err(err) => panic!("{err}"),
+        };
+        let channels = metadata.channels;
+        assert_eq!(channels, 2);
+        assert_eq!(metadata.sample_rate, get_sample_rate());
+        match read_f32_vec_from_file(&audio_path, channels) {
+            Ok(audio_buffer) => {
+                let recovered_sine = audio_buffer.get_buffer(0).unwrap();
+                let recovered_sine = trim_audio(&recovered_sine);
+                let recovered_tri = trim_audio(&audio_buffer.get_buffer(1).unwrap());
+                assert_eq!(recovered_tri.len(), rec_tri.len());
+                assert_eq!(recovered_sine.len(), rec_sine.len());
+                for i in 0..rec_tri.len() {
+                    assert!((rec_tri[i] - recovered_tri[i]).abs() < f32::EPSILON);
                 }
+
+                for i in 0..rec_sine.len() {
+                    assert!((rec_sine[i] - recovered_sine[i]).abs() < f32::EPSILON);
+                }
+            }
+            Err(err) => {
+                panic!(
+                    "* Error: Cannot read data from {audio_path:?}.  Error: {err} audio_duration: {audio_duration}"
+                );
+            }
+        };
+
+        // Test playing back the audio files from the previous test
+        let buffers = vec![
+            Arc::new(Mutex::new(Vec::<f32>::new())),
+            Arc::new(Mutex::new(Vec::<f32>::new())),
+        ];
+        let buffers_new = buffers.to_vec();
+        let port_names = vec!["playback_1".to_string(), "playback_2".to_string()];
+
+        let client_name = "test_play_client";
+        let _ac = make_test_play_client(client_name, port_names, buffers).unwrap();
+        let port_names = _ac
+            .as_client()
+            .ports(Some(client_name), None, PortFlags::IS_INPUT);
+
+        let mut outputs = JackPipes::new(false);
+        for p in port_names.iter() {
+            outputs.add(p).unwrap();
+        }
+
+        let mut app_data = match App::initialise(JackPipes::new(true), outputs, &output_path, true)
+        {
+            Ok(a) => a,
+            Err(err) => panic!("Cannot initalise AppData: {err}"),
+        };
+
+        app_data.handle_kommand(Command::Play).unwrap();
+
+        // Check the buffers are the same
+        let file_path = match app_data.file_manager.make_paths() {
+            Ok(pp) => pp.0,
+            Err(err) => panic!("FileManager.make_paths(): {err}"),
+        };
+        let metadata_path = recorder.file_manager.make_paths().unwrap().1;
+        let metadata: Metadata = match read_file_metadata(metadata_path) {
+            Ok(p) => p,
+            Err(err) => panic!("{err}"),
+        };
+        let channels = metadata.channels;
+        assert_eq!(channels, 2);
+        assert_eq!(metadata.sample_rate, get_sample_rate());
+
+        let audio_buffers = match read_f32_vec_from_file(&file_path, channels) {
+            Ok(p) => p,
+            Err(err) => panic!("{err}"),
+        };
+        let ab_0 = trim_audio(audio_buffers.get_buffer_idx(0).unwrap());
+        {
+            let bf_0 = trim_audio(&buffers_new[0].lock().unwrap());
+            dbg!(&bf_0.len());
+            assert_eq!(ab_0.len() as i32 - bf_0.len() as i32, 0);
+            for idx in 0..ab_0.len() {
+                let a = ab_0[idx];
+                let b = bf_0[idx];
+                assert!((a - b).abs() < f32::EPSILON);
             }
         }
-        if rec_tri.len() != audio_buffer_tri.len().min(rec_tri.len()) {
-            eprintln!(
-                "* Error: Triangle lengths differ: recorded: {} Original: {} audio_duration: {audio_duration}",
-                rec_tri.len(),
-                audio_buffer_tri.len()
-            );
-            result = false;
-        } else {
-            for i in 0..audio_buffer_tri.len() {
-                if (rec_tri[i] - audio_buffer_tri[i]).abs() > f32::EPSILON {
-                    eprintln!("* Error: Triangle differs at {i} audio_duration: {audio_duration}");
-                    result = false;
-                    break;
-                }
+
+        let ab_1 = trim_audio(audio_buffers.get_buffer_idx(1).unwrap());
+        {
+            let bf_1 = trim_audio(&buffers_new[1].lock().unwrap());
+            dbg!(ab_1.len(), bf_1.len());
+            assert_eq!(ab_1.len() as i32 - bf_1.len() as i32, 0);
+            for idx in 0..ab_1.len() {
+                let a = ab_1[idx];
+                let b = bf_1[idx];
+                assert!((a - b).abs() < f32::EPSILON);
             }
         }
-        if result {
-            // Good so far.  The recorded buffers match the input.  Now
-            // check the recorded files are the same
-
-            let audio_path = recorder.file_manager.make_paths().unwrap().0;
-            let metadata_path = recorder.file_manager.make_paths().unwrap().1;
-            let metadata: Metadata = match read_file_metadata(metadata_path) {
-                Ok(p) => p,
-                Err(err) => panic!("{err}"),
-            };
-            let channels = metadata.channels;
-            assert_eq!(channels, 2);
-            assert_eq!(metadata.sample_rate, get_sample_rate());
-            match read_f32_vec_from_file(&audio_path, channels) {
-                Ok(audio_buffer) => {
-                    let recovered_sine = audio_buffer.get_buffer(0).unwrap();
-                    let recovered_sine = trim_audio(&recovered_sine);
-                    let recovered_tri = trim_audio(&audio_buffer.get_buffer(1).unwrap());
-                    if recovered_tri.len() != rec_tri.len() {
-                        eprintln!(
-                            "* Error: recovered_tri.len()/{} != rec_tri.len()/{} audio_duration: {audio_duration}",
-                            recovered_tri.len(),
-                            rec_tri.len()
-                        );
-                        result = false;
-                    } else {
-                        for i in 0..rec_tri.len() {
-                            if (rec_tri[i] - recovered_tri[i]).abs() > f32::EPSILON {
-                                eprintln!(
-                                    "* Error: Recorded tri differs at {i} audio_duration: {audio_duration}"
-                                );
-                                result = false;
-                                break;
-                            }
-                        }
-                    }
-                    if recovered_sine.len() != rec_sine.len() {
-                        eprintln!(
-                            "* Error: recovered_sine.len()/{} != rec_sine.len()/{} audio_duration: {audio_duration}",
-                            recovered_sine.len(),
-                            rec_sine.len()
-                        );
-                        result = false;
-                    } else {
-                        for i in 0..rec_sine.len() {
-                            if (rec_sine[i] - recovered_sine[i]).abs() > f32::EPSILON {
-                                eprintln!(
-                                    "* Error: Recorded sine differs at {i} audio_duration: {audio_duration}"
-                                );
-                                result = false;
-                                break;
-                            }
-                        }
-                    }
-                }
-                Err(err) => {
-                    eprintln!(
-                        "* Error: Cannot read data from {audio_path:?}.  Error: {err} audio_duration: {audio_duration}"
-                    );
-                    result = false;
-                }
-            };
-        }
-        if result {
-            // Test playing back the audio files from the previous test
-            eprint!("* DBG Initialise buffers ");
-            dbg!();
-            let buffers = vec![
-                Arc::new(Mutex::new(Vec::<f32>::new())),
-                Arc::new(Mutex::new(Vec::<f32>::new())),
-            ];
-            let buffers_new = buffers.to_vec();
-            let port_names = vec!["playback_1".to_string(), "playback_2".to_string()];
-
-            let client_name = "test_play_client";
-            let _ac = make_test_play_client(client_name, port_names, buffers).unwrap();
-            let port_names = _ac
-                .as_client()
-                .ports(Some(client_name), None, PortFlags::IS_INPUT);
-
-            let mut outputs = JackPipes::new(false);
-            for p in port_names.iter() {
-                outputs.add(p).unwrap();
-            }
-            let (_audio_tx, _audio_rx) = mpsc::channel::<f32>();
-            let (_command_tx, _command_rx) = mpsc::channel::<Command>();
-
-            let mut app_data = match App::initialise_ui(
-                _command_rx,
-                JackPipes::new(true),
-                outputs,
-                &output_path,
-            ) {
-                Ok(a) => a,
-                Err(err) => panic!("Cannot initalise AppData: {err}"),
-            };
-            app_data.handle_kommand(Command::Play).unwrap();
-
-            // Check the buffers are the same
-            let file_path = match app_data.file_manager.make_paths() {
-                Ok(pp) => pp.0,
-                Err(err) => panic!("{err}"),
-            };
-            let metadata_path = recorder.file_manager.make_paths().unwrap().1;
-            let metadata: Metadata = match read_file_metadata(metadata_path) {
-                Ok(p) => p,
-                Err(err) => panic!("{err}"),
-            };
-            let channels = metadata.channels;
-            assert_eq!(channels, 2);
-            assert_eq!(metadata.sample_rate, get_sample_rate());
-
-            let audio_buffers = match read_f32_vec_from_file(&file_path, channels) {
-                Ok(p) => p,
-                Err(err) => panic!("{err}"),
-            };
-            let ab_0 = trim_audio(audio_buffers.get_buffer_idx(0).unwrap());
-            {
-                let bf_0 = trim_audio(&buffers_new[0].lock().unwrap());
-                if ab_0.len() != bf_0.len() {
-                    eprint!(
-                        "* Error: {} != {} audio_duration: {audio_duration} ",
-                        &ab_0.len(),
-                        &bf_0.len()
-                    );
-                    dbg!();
-                    result = false;
-                } else {
-                    for idx in 0..ab_0.len() {
-                        let a = ab_0[idx];
-                        let b = bf_0[idx];
-                        if (a - b).abs() >= f32::EPSILON {
-                            result = false;
-                            eprintln!(
-                                "* Error: channel 0: Failed match @ {idx}: Saved: {a:0.4}  Buffered: {b:0.4} audio_duration: {audio_duration}"
-                            );
-                            break;
-                        }
-                    }
-                }
-            }
-            let ab_1 = trim_audio(audio_buffers.get_buffer_idx(1).unwrap());
-            {
-                let bf_1 = trim_audio(&buffers_new[1].lock().unwrap());
-                if ab_1.len() != bf_1.len() {
-                    eprint!(
-                        "* Error: Channel 1 recorded audio does not equal played back audio: {} != {} audio_duration: {audio_duration} ",
-                        &ab_1.len(),
-                        &bf_1.len()
-                    );
-                    dbg!();
-                    result = false;
-                } else {
-                    for idx in 0..ab_1.len() {
-                        let a = ab_1[idx];
-                        let b = bf_1[idx];
-                        if (a - b).abs() >= f32::EPSILON {
-                            eprintln!(
-                                "* Error: channel 1: Failed match @ {idx}: Saved: {a}  Buffered: {b} audio_duration: {audio_duration}"
-                            );
-                        }
-                    }
-                }
-            }
-        }
-        assert!(result);
     }
 }
 
