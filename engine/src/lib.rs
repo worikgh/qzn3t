8dece606f1059a1e7cee64ee247c196084445915
Worik <5387413+worikgh@users.noreply.github.com>
2026-03-06 14:10:51 +1300

    Jack code mved to its own unit

--- engine/src/lib.rs
+++ engine/src/lib.rs
@@ -1,7 +1,7 @@
 // Copyright (c) 2026 Worik Turei Stanton
 // License: GPL-3.0
 
-use jack::{AsyncClient, AudioIn, AudioOut, Client, ClientOptions, Port, PortFlags, Unowned};
+use jack::{AsyncClient, AudioIn, AudioOut, Client, ClientOptions};
 #[allow(unused_imports)]
 use qzn3t_audio_buffer::AudioBuffer;
 use qzn3terror::Qzn3tError;
@@ -15,6 +15,9 @@ use std::{
 	mpsc,
     },
 };
+
+use crate::process_audio::{Notifications, ProcessAudio};
+mod process_audio;
 
 #[derive(Debug)]
 #[allow(dead_code)]
@@ -136,129 +139,18 @@ impl Engine {
 	    ports_receivers.push((port, rx));
 	}
 	Ok((
-	    ProcessAudio {
+	    ProcessAudio::new(
 		run_f,
 		// Get data from/ send data to owner
 		ports_receivers,
 		ports_senders,
-	    },
+	    ),
 	    // For owner to use to send/reveive data
 	    senders,
 	    receivers,
 	))
     }
-
 }
 
 //-------------------
 // Jackd code: TODO: move this to its own unit
-pub struct Notifications;
-impl jack::NotificationHandler for Notifications {
-    fn thread_init(&self, _: &jack::Client) {
-	// eprintln!("DBG NotificationHandler thread_init");
-    }
-    unsafe fn shutdown(&mut self, _status: jack::ClientStatus, _reason: &str) {
-	// eprintln!("DBG NotificationHandler shutdown {_status:?} {_reason}");
-    }
-    fn freewheel(&mut self, _: &jack::Client, _is_freewheel_enabled: bool) {
-	// eprintln!("DBG NotificationHandler freewheel {_is_freewheel_enabled}");
-    }
-    fn client_registration(&mut self, _: &jack::Client, _name: &str, _is_registered: bool) {
-	// eprintln!("DBG NotificationHandler client_registration: {_name}/{_is_registered}");
-    }
-    fn port_registration(
-	&mut self,
-	_: &jack::Client,
-	_port_id: jack::PortId,
-	_is_registered: bool,
-    ) {
-	// eprintln!("DBG NotificationHandler port_registration: {_port_id}/{_is_registered}");
-    }
-    fn port_rename(
-	&mut self,
-	_: &jack::Client,
-	_port_id: jack::PortId,
-	_old_name: &str,
-	_new_name: &str,
-    ) -> jack::Control {
-	// eprintln!("DBG NotificationHandler port_rename: {_port_id} {_old_name} -> {_new_name}");
-	jack::Control::Continue
-    }
-    fn ports_connected(
-	&mut self,
-	_: &jack::Client,
-	_port_id_a: jack::PortId,
-	_port_id_b: jack::PortId,
-	_are_connected: bool,
-    ) {
-	// eprintln!(
-	//     "DBG NotificationHandler: ports_connected {_port_id_a}/{_port_id_b} {_are_connected}"
-	// );
-    }
-    fn graph_reorder(&mut self, _: &jack::Client) -> jack::Control {
-	// eprintln!("DBG NotificationHandler graph_reorder");
-	jack::Control::Continue
-    }
-
-    fn xrun(&mut self, _: &jack::Client) -> jack::Control {
-	eprintln!("Error NotificationHandler xrun");
-	jack::Control::Continue
-    }
-
-    fn sample_rate(&mut self, _: &jack::Client, _srate: jack::Frames) -> jack::Control {
-	jack::Control::Continue
-    }
-}
-
-/// Handler for audio output.  Receive multi-channel audio data on a
-/// set of `mpsc::Channel`s and send them to matching Jack ports
-#[derive(Debug)]
-pub struct ProcessAudio {
-    run_f: Arc<AtomicBool>,
-    /// Receive Audiop from the code that owns this `Engine` and send
-    /// it out on Jack outputs
-    ports_receivers: Vec<(jack::Port<jack::AudioOut>, mpsc::Receiver<f32>)>,
-    /// Receive Audiop on Jack inputs and send it to the code that
-    /// owns this `Engine`
-    ports_senders: Vec<(jack::Port<jack::AudioIn>, mpsc::Sender<f32>)>,
-}
-
-impl jack::ProcessHandler for ProcessAudio {
-    /// Full duplex audio.  Audio Output: If there are audio data in
-    /// the receiver channels send it out on the associated port, if
-    /// no audio available send 0_f32.  Audio Input: Any data
-    /// available on the input jack ports send them out on the sender
-    /// channels
-    fn process(&mut self, _c: &jack::Client, ps: &jack::ProcessScope) -> jack::Control {
-	for (port, receiver) in self.ports_receivers.iter_mut() {
-	    let out = port.as_mut_slice(ps);
-	    for s in out.iter_mut() {
-		*s = match receiver.try_recv() {
-		    Ok(s) => s,
-		    Err(mpsc::TryRecvError::Empty) => 0.0,
-		    Err(mpsc::TryRecvError::Disconnected) => {
-			eprintln!("jack_rec. Error: Disconnected audio channel");
-			return jack::Control::Quit;
-		    }
-		};
-	    }
-	}
-	for (port, sender) in self.ports_senders.iter_mut() {
-	    let in_a_p: &[f32] = port.as_slice(ps);
-	    for v in in_a_p {
-		if let Err(err) = sender.send(*v) {
-		    eprintln!(
-			"Error jack_rec: Cannot send data ({v}) through a channel. Error: {err} "
-		    );
-		    return jack::Control::Quit;
-		}
-	    }
-	}
-
-	if !self.run_f.load(Ordering::SeqCst) {
-	    jack::Control::Quit
-	} else {
-	    jack::Control::Continue
-	}
-    }
-}
---
+++ engine/src/process_audio.rs
@@ -0,0 +1,135 @@
+// Copyright (c) 2026 Worik Turei Stanton
+// License: GPL-3.0
+
+//! Handler for audio INPUT AND output.  Receive multi-channel audio
+//! data on a set of `mpsc::Channel`s and send them to matching Jack
+//! ports
+
+// use jack::{AsyncClient, AudioIn, AudioOut, Client, ClientOptions, Port, PortFlags, Unowned};
+use std::sync::{
+    Arc,
+    atomic::{AtomicBool, Ordering},
+    mpsc,
+};
+
+#[derive(Debug)]
+pub struct ProcessAudio {
+    run_f: Arc<AtomicBool>,
+    /// Receive Audiop from the code that owns this `Engine` and send
+    /// it out on Jack outputs
+    ports_receivers: Vec<(jack::Port<jack::AudioOut>, mpsc::Receiver<f32>)>,
+    /// Receive Audiop on Jack inputs and send it to the code that
+    /// owns this `Engine`
+    ports_senders: Vec<(jack::Port<jack::AudioIn>, mpsc::Sender<f32>)>,
+}
+
+impl ProcessAudio {
+    pub fn new(
+        run_f: Arc<AtomicBool>,
+        ports_receivers: Vec<(jack::Port<jack::AudioOut>, mpsc::Receiver<f32>)>,
+        ports_senders: Vec<(jack::Port<jack::AudioIn>, mpsc::Sender<f32>)>,
+    ) -> Self {
+        Self {
+            run_f,
+            ports_receivers,
+            ports_senders,
+        }
+    }
+}
+impl jack::ProcessHandler for ProcessAudio {
+    /// Full duplex audio.  Audio Output: If there are audio data in
+    /// the receiver channels send it out on the associated port, if
+    /// no audio available send 0_f32.  Audio Input: Any data
+    /// available on the input jack ports send them out on the sender
+    /// channels
+    fn process(&mut self, _c: &jack::Client, ps: &jack::ProcessScope) -> jack::Control {
+        for (port, receiver) in self.ports_receivers.iter_mut() {
+            let out = port.as_mut_slice(ps);
+            for s in out.iter_mut() {
+                *s = match receiver.try_recv() {
+                    Ok(s) => s,
+                    Err(mpsc::TryRecvError::Empty) => 0.0,
+                    Err(mpsc::TryRecvError::Disconnected) => {
+                        eprintln!("jack_rec. Error: Disconnected audio channel");
+                        return jack::Control::Quit;
+                    }
+                };
+            }
+        }
+        for (port, sender) in self.ports_senders.iter_mut() {
+            let in_a_p: &[f32] = port.as_slice(ps);
+            for v in in_a_p {
+                if let Err(err) = sender.send(*v) {
+                    eprintln!(
+                        "Error jack_rec: Cannot send data ({v}) through a channel. Error: {err} "
+                    );
+                    return jack::Control::Quit;
+                }
+            }
+        }
+
+        if !self.run_f.load(Ordering::Relaxed) {
+            jack::Control::Quit
+        } else {
+            jack::Control::Continue
+        }
+    }
+}
+
+pub struct Notifications;
+impl jack::NotificationHandler for Notifications {
+    fn thread_init(&self, _: &jack::Client) {
+        // eprintln!("DBG NotificationHandler thread_init");
+    }
+    unsafe fn shutdown(&mut self, _status: jack::ClientStatus, _reason: &str) {
+        // eprintln!("DBG NotificationHandler shutdown {_status:?} {_reason}");
+    }
+    fn freewheel(&mut self, _: &jack::Client, _is_freewheel_enabled: bool) {
+        // eprintln!("DBG NotificationHandler freewheel {_is_freewheel_enabled}");
+    }
+    fn client_registration(&mut self, _: &jack::Client, _name: &str, _is_registered: bool) {
+        // eprintln!("DBG NotificationHandler client_registration: {_name}/{_is_registered}");
+    }
+    fn port_registration(
+        &mut self,
+        _: &jack::Client,
+        _port_id: jack::PortId,
+        _is_registered: bool,
+    ) {
+        // eprintln!("DBG NotificationHandler port_registration: {_port_id}/{_is_registered}");
+    }
+    fn port_rename(
+        &mut self,
+        _: &jack::Client,
+        _port_id: jack::PortId,
+        _old_name: &str,
+        _new_name: &str,
+    ) -> jack::Control {
+        // eprintln!("DBG NotificationHandler port_rename: {_port_id} {_old_name} -> {_new_name}");
+        jack::Control::Continue
+    }
+    fn ports_connected(
+        &mut self,
+        _: &jack::Client,
+        _port_id_a: jack::PortId,
+        _port_id_b: jack::PortId,
+        _are_connected: bool,
+    ) {
+        // eprintln!(
+        //     "DBG NotificationHandler: ports_connected {_port_id_a}/{_port_id_b} {_are_connected}"
+        // );
+    }
+    fn graph_reorder(&mut self, _: &jack::Client) -> jack::Control {
+        // eprintln!("DBG NotificationHandler graph_reorder");
+        jack::Control::Continue
+    }
+
+    fn xrun(&mut self, _: &jack::Client) -> jack::Control {
+        eprintln!("Error NotificationHandler xrun");
+        jack::Control::Continue
+    }
+
+    fn sample_rate(&mut self, _: &jack::Client, _srate: jack::Frames) -> jack::Control {
+        jack::Control::Continue
+    }
+}
